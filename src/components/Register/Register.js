824629a1bd4c86494a1caf05172a71fbb76c7ac2
Nokkvi Sverrisson <nokkvisverriss@gmail.com>
2019-05-02 22:21:52 +0000

    User now gets a session on registration

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,6 +22,10 @@ class Register extends React.Component {
 		this.setState({password: event.target.value})
 	}
 
+	saveAuthTokenInSession = (token) => {
+		window.sessionStorage.setItem('token', token);
+	}
+
 	onSubmitRegistration = () => {
 		fetch(process.env.REACT_APP_API_URL+"/register", {
 			method: 'post',
@@ -33,10 +37,23 @@ class Register extends React.Component {
 			})
 		})
 			.then(response => response.json())
-			.then(user => {
-				if (user.id){
-					this.props.loadUser(user);
-					this.props.onRouteChange('home');
+			.then(data => {
+				if (data.userId && data.success === 'true'){
+					this.saveAuthTokenInSession(data.token);
+					fetch(`${process.env.REACT_APP_API_URL}/profile/${data.userId}`, {
+						method: 'get',
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': data.token
+						}
+					})
+					.then(resp => resp.json())
+					.then(user => {
+						if(user && user.email) {
+							this.props.loadUser(user);
+							this.props.onRouteChange('home');
+						}
+					})
 				}
 			})
 	}
